2f174f7d9531ff904846748f516a99a09bf597c3
m1a2st <s7133700@gmail.com>
2023-02-22 17:03:44 +0800

    feat : add Security config login success handler and login fail Handler

--- src/main/java/com/secpractice/securitypractice/config/SecurityConfig.java
+++ src/main/java/com/secpractice/securitypractice/config/SecurityConfig.java
@@ -1,24 +1,17 @@
 package com.secpractice.securitypractice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.Filter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import static com.secpractice.securitypractice.utils.ResponseUtils.responseJsonWriter;
 
 /**
  * @Author
@@ -27,11 +20,17 @@ import java.io.PrintWriter;
  * @Description
  */
 @Configuration
+@Log4j2
 @EnableWebSecurity
 public class SecurityConfig{
 
-    public SecurityConfig() {
+    private final AuthenticationSuccessHandler customerASHandler;
+    private final AuthenticationFailureHandler customerAFHandler;
+
+    public SecurityConfig(AuthenticationFailureHandler customerAFHandler, AuthenticationSuccessHandler customerASHandler) {
         super();
+        this.customerAFHandler = customerAFHandler;
+        this.customerASHandler = customerASHandler;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -39,12 +38,13 @@ public class SecurityConfig{
                 .cors()
                 .and()
                 .authorizeHttpRequests()
+                .antMatchers("/login","/login/failure").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginProcessingUrl("/process")
-                .successForwardUrl("/login/success")
-                .failureForwardUrl("/login/failure")
+                .loginProcessingUrl("/login")
+                .successHandler(customerASHandler)
+                .failureHandler(customerAFHandler)
                 .and()
                 .logout()
                 .logoutUrl("/logout")
@@ -57,33 +57,13 @@ public class SecurityConfig{
     }
 
     public LogoutHandler customerLogoutHandler(){
-        return new LogoutHandler() {
-            @Override
-            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
-            }
+        return (request, response, authentication) -> {
+            log.info("logout handler is running....");
         };
     }
     @Bean
     public LogoutSuccessHandler customerLogoutSuccessHandler () {
-        return new LogoutSuccessHandler() {
-            @Override
-            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
-            }
-
-            private static void responseJsonWriter(HttpServletResponse response,String msg) throws IOException {
-                response.setStatus(HttpServletResponse.SC_OK);
-                response.setCharacterEncoding("utf-8");
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                ObjectMapper objectMapper = new ObjectMapper();
-                String resBody = objectMapper.writeValueAsString(msg);
-                PrintWriter printWriter = response.getWriter();
-                printWriter.print(resBody);
-                printWriter.flush();
-                printWriter.close();
-            }
-        };
+        return (request, response, authentication) -> responseJsonWriter(response, authentication.getName());
     }
 
 
--- src/main/java/com/secpractice/securitypractice/config/WebBean.java
+++ src/main/java/com/secpractice/securitypractice/config/WebBean.java
@@ -1,11 +1,27 @@
 package com.secpractice.securitypractice.config;
 
+import com.secpractice.securitypractice.utils.JwtTokenUtils;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.secpractice.securitypractice.utils.ResponseUtils.responseJsonWriter;
 
 /**
  * @Author
@@ -14,9 +30,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * @Description
  */
 @Configuration
+@Log4j2
 public class WebBean {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -32,5 +49,27 @@ public class WebBean {
                 .roles("USER", "ADMIN")
                 .build());
         return manager;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customerASHandler() {
+        return (request, response, authentication) -> {
+            if (response.isCommitted()) {
+                log.debug("Response is already commit");
+            }
+            Map<String, String> map = new HashMap<>();
+            map.put("time", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            map.put("msg", "success_login");
+            map.put("name", authentication.getName());
+//            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//            Set<String> role = authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+            String token = JwtTokenUtils.generateToken(map);
+            responseJsonWriter(response, token);
+        };
+    }
+
+    @Bean
+    public AuthenticationFailureHandler customerAfHandler() {
+        return (request, response, exception) -> responseJsonWriter(response,"login fail");
     }
 }
