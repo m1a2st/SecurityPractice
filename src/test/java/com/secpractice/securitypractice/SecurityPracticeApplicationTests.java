fdabb022d1ef5dbeb11ac8a8440b3cae4c1d0a7a
m1a2st <s7133700@gmail.com>
2023-02-22 16:56:32 +0800

    feat : add JWT token Test

--- src/test/java/com/secpractice/securitypractice/SecurityPracticeApplicationTests.java
+++ src/test/java/com/secpractice/securitypractice/SecurityPracticeApplicationTests.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,13 +25,34 @@ class SecurityPracticeApplicationTests {
     }
 
     @Test
-    void test_login_page() throws Exception {
-        mockMvc.perform(post("/process"))
+    void login_page() throws Exception {
+        mockMvc.perform(post("/login"))
                 .andExpect(status().isOk());
     }
 
-//    @Test login_fail(){
-//        mockMvc.perform(post())
-//    }
+    @Test
+    @WithUserDetails(value = "admin")
+    void login() throws Exception {
+        mockMvc.perform(post("/login"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin")
+    void login_logout() throws Exception {
+        mockMvc.perform(post("/login"))
+                .andExpect(status().isOk());
+        mockMvc.perform(post("/logout"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails()
+    void login_fail() throws Exception {
+        String failMessage = mockMvc.perform(post("/login"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("\"login fail\"", failMessage);
+    }
 
 }
