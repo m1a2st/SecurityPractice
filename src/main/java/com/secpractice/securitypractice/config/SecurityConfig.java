c2d36fa6c2b3cb16457f47da9fb7fd22efa71403
m1a2st <s7133700@gmail.com>
2023-02-22 14:59:10 +0800

    feat : add new Logout Handler and new LogoutSuccessHandler

--- src/main/java/com/secpractice/securitypractice/config/SecurityConfig.java
+++ src/main/java/com/secpractice/securitypractice/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.secpractice.securitypractice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @Author
@@ -34,8 +44,46 @@ public class SecurityConfig{
                 .formLogin()
                 .loginProcessingUrl("/process")
                 .successForwardUrl("/login/success")
-                .failureForwardUrl("/login/failure");
+                .failureForwardUrl("/login/failure")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .deleteCookies("CookieName")
+                .invalidateHttpSession(true)
+                .addLogoutHandler(customerLogoutHandler())
+                .logoutSuccessHandler(customerLogoutSuccessHandler());
         return http.build();
+    }
+
+    public LogoutHandler customerLogoutHandler(){
+        return new LogoutHandler() {
+            @Override
+            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+            }
+        };
+    }
+    @Bean
+    public LogoutSuccessHandler customerLogoutSuccessHandler () {
+        return new LogoutSuccessHandler() {
+            @Override
+            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+            }
+
+            private static void responseJsonWriter(HttpServletResponse response,String msg) throws IOException {
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.setCharacterEncoding("utf-8");
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                ObjectMapper objectMapper = new ObjectMapper();
+                String resBody = objectMapper.writeValueAsString(msg);
+                PrintWriter printWriter = response.getWriter();
+                printWriter.print(resBody);
+                printWriter.flush();
+                printWriter.close();
+            }
+        };
     }
 
 
