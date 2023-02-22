368ef4d287f62c6a0d56aec43625d7629946abba
m1a2st <s7133700@gmail.com>
2023-02-22 16:57:07 +0800

    refactor : Login controller duplicate

--- src/main/java/com/secpractice/securitypractice/controller/LoginController.java
+++ src/main/java/com/secpractice/securitypractice/controller/LoginController.java
@@ -16,31 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version
  * @Description
  */
-@RestController
-@RequestMapping("/login")
-public class LoginController {
-
-    private final UserService userService;
-
-    public LoginController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping("/failure")
-    public ResponseEntity<UserView> loginFail(){
-        return new ResponseEntity<>(UserView.builder()
-                .message("login fail....")
-                .build(), HttpStatus.UNAUTHORIZED);
-    }
-
-    @PostMapping("/success")
-    public ResponseEntity<UserView> loginSuccess(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(authentication.getName());
-        System.out.println(authentication.getAuthorities());
-        System.out.println(authentication.getCredentials());
-        System.out.println(authentication.getDetails());
-        System.out.println(authentication.getPrincipal());
-        return new ResponseEntity<>(new UserView(),HttpStatus.OK);
-    }
-}
+//@RestController
+//@RequestMapping("/login")
+//public class LoginController {
+//
+//    private final UserService userService;
+//
+//    public LoginController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @PostMapping("/failure")
+//    public ResponseEntity<UserView> loginFail(){
+//        return new ResponseEntity<>(UserView.builder()
+//                .message("login fail....")
+//                .build(), HttpStatus.UNAUTHORIZED);
+//    }
+//
+//    @PostMapping("/success")
+//    public ResponseEntity<UserView> loginSuccess(){
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        System.out.println(authentication.getName());
+//        System.out.println(authentication.getAuthorities());
+//        System.out.println(authentication.getCredentials());
+//        System.out.println(authentication.getDetails());
+//        System.out.println(authentication.getPrincipal());
+//        return new ResponseEntity<>(new UserView(),HttpStatus.OK);
+//    }
+//}
